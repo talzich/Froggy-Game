86b814afdf0035b21f67865c447afdcbee4aca08
Tal <talzhtc@gmail.com>
2021-11-10 09:30:55 +0200

    Part B

---
+++ Assets/Scripts/1-movers/KeyboardMover.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This component moves its object when the player clicks the arrow keys.
+ */
+public class KeyboardMover: MonoBehaviour {
+    [Tooltip("Speed of movement, in meters per second")]
+    [SerializeField] float speed = 1f;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(new Vector3(0, 2f, 0));
+        }
+    }
+}
---
+++ Assets/Scripts/3-collisions/CelebrateOnSafety.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This component destroys its object whenever it triggers a 2D collider with the given tag.
+ */
+public class CelebrateOnSafety : MonoBehaviour {
+
+    [Tooltip("Objects tagged with this tag will make the win the level on collision")]
+    [SerializeField] string triggeringTag;
+    [SerializeField] private float speed;
+    private bool safe = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        safe = true;
+        if (other.tag == triggeringTag && enabled)
+        {
+            safe = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (safe)
+        {
+
+            KeyboardMover keyboardMover = GetComponent<KeyboardMover>();
+            keyboardMover.enabled = false;
+            float x = (float)Mathf.Sin(speed*Time.time);
+            float y = transform.position.y;
+            float z = transform.position.z;
+            transform.position = new Vector3(x, y, z);
+        }
+    }
+}
